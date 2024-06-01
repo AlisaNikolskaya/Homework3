3174388aaefda4e2b15157877fcb142b7b67794e
AlisaNikolskaya <nikolalisa13@gmail.com>
2024-06-01 20:15:44 +0300

    Add files via upload

---
+++ homework3.py
@@ -0,0 +1,8 @@
+name = 'Alisa'
+print('Name' + ':', name, type(name))
+age = 28
+print('Age' + ':', str(age))
+age = age + 5
+print('New age' + ':', str(age))
+is_student = True
+print('Is Student' + ':', str(is_student))
